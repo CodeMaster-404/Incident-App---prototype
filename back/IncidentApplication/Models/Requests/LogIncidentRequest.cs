9d0685666ce507659c0a13f9e30ce2d733e1d2d9
CodeMaster-404 <74175097+CodeMaster-404@users.noreply.github.com>
2021-02-23 09:17:18 +0200

    Add files via upload

---
+++ back/IncidentApplication/Models/Requests/LogIncidentRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IncidentApplication.Models.Requests
+{
+    public class LogIncidentRequest
+    {
+        
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public DateTime Date_Logged { get; set; }
+
+        [Required]
+        public int StatusId { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+    }
+}
